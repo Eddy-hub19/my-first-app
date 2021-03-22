7ada85cd609bff5ebcf2458ec77f445b96a55b24
Eddy <you@example.com>
2021-03-22 22:18:09 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import User from "./components/user/User";
 
+/*JSX*/
 function App() {
+  let users = [
+    {
+      name: "vasya",
+      age: 31,
+      status: false,
+      address: {
+        city: "Lviv",
+        country: "Ukraine",
+        street: "Shevchenko",
+        houseNumber: 1,
+      },
+    },
+    {
+      name: "petya",
+      age: 30,
+      status: true,
+      address: {
+        city: "New York",
+        country: "USA",
+        street: "East str",
+        houseNumber: 21,
+      },
+    },
+    {
+      name: "kolya",
+      age: 29,
+      status: true,
+      address: {
+        city: "Budapest",
+        country: "Hungary",
+        street: "Kuraku",
+        houseNumber: 78,
+      },
+    },
+    {
+      name: "olya",
+      age: 28,
+      status: false,
+      address: {
+        city: "Prague",
+        country: "Czech",
+        street: "Paster",
+        houseNumber: 56,
+      },
+    },
+    {
+      name: "max",
+      age: 30,
+      status: true,
+      address: {
+        city: "Istanbul",
+        country: "Turkey",
+        street: "Mikar",
+        houseNumber: 39,
+      },
+    },
+    {
+      name: "anya",
+      age: 31,
+      status: false,
+      address: {
+        city: "Rio",
+        country: "Brasil",
+        street: "Ronaldi",
+        houseNumber: 5,
+      },
+    },
+    {
+      name: "oleg",
+      age: 28,
+      status: false,
+      address: {
+        city: "Montreal",
+        country: "Canada",
+        street: "Acusto",
+        houseNumber: 90,
+      },
+    },
+    {
+      name: "andrey",
+      age: 29,
+      status: true,
+      address: {
+        city: "Quebeck",
+        country: "Canada",
+        street: "Binaro",
+        houseNumber: 33,
+      },
+    },
+    {
+      name: "masha",
+      age: 30,
+      status: true,
+      address: {
+        city: "Moscow",
+        country: "Russia",
+        street: "Gogolia",
+        houseNumber: 1,
+      },
+    },
+    {
+      name: "olya",
+      age: 31,
+      status: false,
+      address: {
+        city: "Portland",
+        country: "USA",
+        street: "Forest str",
+        houseNumber: 4,
+      },
+    },
+    {
+      name: "max",
+      age: 31,
+      status: true,
+      address: {
+        city: "Cairo",
+        country: "Egypt",
+        street: "Seashore",
+        houseNumber: 45,
+      },
+    },
+  ];
+
+  //треба добавляти ключ,якщо хочеш,щоб реакт міняв тільки одне значення
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((value,index) => (<User user={value} key={index} />))} 
     </div>
   );
 }
